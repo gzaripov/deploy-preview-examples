7affa0123a7cae2fa8874cf3fdfd3286fb38e05e
Grigory Zaripov <grigory.zaripov@gmail.com>
2022-06-13 15:28:23 +0300

    chore: fix branch name for deployment

--- simple/frontend/deploy.sh
+++ simple/frontend/deploy.sh
@@ -1,6 +1,6 @@
 set -e
 
-BRANCH=`git branch --show-current`
+BRANCH=`git rev-parse --abbrev-ref HEAD`
 
 s3cmd put -r dist/ s3://frontend-static/${BRANCH}/
 s3cmd put -r dist/index.html s3://frontend-manifests/${BRANCH}/
