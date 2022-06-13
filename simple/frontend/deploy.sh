0953e62b5d5107da58f351afbe56bc19b5c47647
Grigory Zaripov <grigory.zaripov@gmail.com>
2022-06-13 15:39:48 +0300

    chore: test

--- simple/frontend/deploy.sh
+++ simple/frontend/deploy.sh
@@ -2,7 +2,7 @@ set -e
 
 : ${BRANCH:=`git rev-parse --abbrev-ref HEAD`}
 
-s3cmd put -r dist/ s3://frontend-static/${BRANCH}/
-s3cmd put -r dist/index.html s3://frontend-manifests/${BRANCH}/
+s3cmd put -r dist s3://frontend-static/${BRANCH}/
+s3cmd put -r dist/index.html -f s3://frontend-manifests/${BRANCH}/
 
 echo "Site is deployed to ${CDN_URL}${BRANCH}/"
