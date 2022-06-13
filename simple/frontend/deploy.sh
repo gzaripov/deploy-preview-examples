9d7b6e87a3101d30a57aa04c2ab7487cde1cccbd
Grigory Zaripov <grigory.zaripov@gmail.com>
2022-06-13 16:53:23 +0300

    chore: remove python3-magic

--- simple/frontend/deploy.sh
+++ simple/frontend/deploy.sh
@@ -2,6 +2,6 @@ set -e
 
 : ${BRANCH:=`git rev-parse --abbrev-ref HEAD`}
 
-s3cmd put --no-mime-magic --guess-mime-type -r --force dist/ s3://frontend-static/${BRANCH}/
+s3cmd put -r --force dist/ s3://frontend-static/${BRANCH}/
 
 echo "Site is deployed to ${CDN_URL}${BRANCH}/"
